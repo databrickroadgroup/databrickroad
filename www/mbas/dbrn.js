70c2f0b9e0cd095873f71c3a62ca54e6f0402ce0
mbeezy <bolanos.mike@gmail.com>
2016-09-18 21:05:20 -0700

    complete the native js re-write

--- www/mbas/dbrn.js
+++ www/mbas/dbrn.js
@@ -1,6 +1,14 @@
 (function(){
 
-  const cookieConst = 'dbru';
+  const USRCOOKIE = 'dbru';
+  const PGLOADEVT = 'pageload';
+  const PGSCROLLEVT = 'pagescroll';
+  const USRCTRL = 'user';
+  const API = 'http://databrickroad.local/api';
+  const APIPOST = 'POST';
+  const APIGET = 'GET';
+  const APIRESPONSETYPE = 'json';
+
   var pageDomain = document.domain;
   var pageLocation = window.location.pathname;
   var referrer = document.referrer;
@@ -8,21 +16,61 @@
   var firstQuarterMet, halfwayMet, threeQuarterMet, bottomMet = false;
 
   document.addEventListener('DOMContentLoaded', function () {
-      console.log('Aloha');
-      console.log(pageDomain);
-      processEvent(callback);
+      processEvent(callback, PGLOADEVT);
+  });
+
+  window.addEventListener('scroll', function (e) {
+
+    var scrollAmount = window.innerHeight + window.scrollY;
+    var documentHeight = $(document).height();
+    var scrollPercent = (scrollAmount / documentHeight) * 100;
+
+    if ((!firstQuarterMet) && (scrollPercent > 25)) {
+      firstQuarterMet = true;
+      console.log('1A');
+      processEvent(callback, PGSCROLLEVT, '1/4');
+    }
+    if ((!halfwayMet) && (scrollPercent > 50)) {
+  		halfwayMet = true;
+  		console.log('2A');
+  		processEvent(callback, PGSCROLLEVT, '2/4');
+  	}
+
+  	if ((!threeQuarterMet) && (scrollPercent > 75)) {
+  		threeQuarterMet = true;
+  		console.log('3A');
+  		processEvent(callback, PGSCROLLEVT, '3/4');
+  	}
+
+  	if ((!bottomMet) && (scrollPercent > 90)) {
+  		bottomMet = true;
+  		console.log('4A');
+  		processEvent(callback, PGSCROLLEVT, '4/4');
+  	}
+
   });
 
   function processEvent(callback, eventType, eventDetail) {
-    console.log('processing...');
-    var cookie = 'blah134kd';
 
-    if (!doesCookieExist(cookieConst)) {
-      setCookie(cookieConst, cookie, 1825); // 5 yrs
+    var proxy = API + '/' + eventType + '/' + USRCTRL;
+    var created = getCurrentDateTime();
+    var user_guid = getCookie(USRCOOKIE);
+
+    var postData;
+    if (eventType == PGLOADEVT) {
+      postData = { "created":created, "user_guid":user_guid, "domain":pageDomain, "page_name":pageLocation, "page_url":page_url, "referrer":referrer};
+    } else if (eventType == PGSCROLLEVT) {
+      postData = { "created":created, "user_guid":user_guid, "domain":pageDomain, "page_name":pageLocation, "page_url":page_url, "referrer":referrer, "page_position_code":eventDetail};
     }
 
-    var cookiemookie = getCookie(cookieConst);
-    callback(cookiemookie, 'test2');
+    postEvent(proxy, APIPOST, postData, function(data) {
+      if (!doesCookieExist(USRCOOKIE)) {
+        console.log('setting cookie');
+        setCookie(USRCOOKIE, data.databrickuser, 1825);
+      }
+
+      callback(data.databrickuser, data.result);
+    });
   }
 
   function setCookie(name, value, days) {
@@ -39,6 +87,7 @@
 
   function getCookie(name) {
     var cookie_array = document.cookie.match( '(^|;) ?' + name + '=([^;]*)(;|$)' );
+    var value = '';
 
     if (cookie_array) {
       var value = cookie_array[2];
@@ -54,5 +103,24 @@
       return false;
     }
   }
+
+  function getCurrentDateTime() {
+    var date = new Date();
+  	return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  }
+
+  // posting to the API
+  var postEvent = function(url, type, data, responseHandler, errorHandler) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(type, url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+    xhr.onreadystatechange = function(){
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        var response = JSON.parse(xhr.responseText);
+        responseHandler(response);
+      }
+    };
+  };
 
 })();
